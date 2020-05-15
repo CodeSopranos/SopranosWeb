16990bcd291f4e3a8ead8a65bf802e4460ae8707
Vadik Alperovich <vadikalperovich@mail.ru>
2020-05-16 02:41:24 +0300

    minor fix and initial migrations

--- app/board/models.py
+++ app/board/models.py
@@ -1,3 +1,18 @@
-from django.db import models
+from django.db.models import *
 
-# Create your models here.
+
+class Blog(Model):
+    title = CharField(max_length=80)
+    created_at = DateTimeField('creation timestamp', auto_now_add=True)
+    
+    def __str__(self):
+        return str(self.title)
+
+
+
+class Post(Model):
+    blog = ForeignKey(Blog, on_delete=CASCADE)
+    subject = CharField(max_length=80)
+    text = TextField(max_length=4096)
+    created_at = DateTimeField('creation timestamp', auto_now_add=True)
+    updated_at = DateTimeField('update timestamp', auto_now=True)
