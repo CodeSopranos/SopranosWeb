08aba062c0fac5021a017705e7d76e48cbe4abd1
Vadik Alperovich <vadikalperovich@mail.ru>
2020-05-18 02:50:06 +0300

    minor fixes

--- app/board/models.py
+++ app/board/models.py
@@ -1,18 +1,24 @@
+from django.contrib.postgres.fields import JSONField
 from django.db.models import *
 
 
-class Blog(Model):
+class Dashboard(Model):
+    id = AutoField(primary_key=True)
     title = CharField(max_length=80)
     created_at = DateTimeField('creation timestamp', auto_now_add=True)
-    
+    user_id = IntegerField(default=0)
+
     def __str__(self):
         return str(self.title)
 
 
-
-class Post(Model):
-    blog = ForeignKey(Blog, on_delete=CASCADE)
-    subject = CharField(max_length=80)
-    text = TextField(max_length=4096)
-    created_at = DateTimeField('creation timestamp', auto_now_add=True)
-    updated_at = DateTimeField('update timestamp', auto_now=True)
+class Figure(Model):
+    id = AutoField(primary_key=True)
+    dashboard = ForeignKey(Dashboard, on_delete=CASCADE)
+    tag = CharField(max_length=80)
+    data = JSONField()
+    params = JSONField()
+    modify_at = DateTimeField('creation timestamp', auto_now_add=True)
+    
+    def __str__(self):
+        return str(self.tag)
