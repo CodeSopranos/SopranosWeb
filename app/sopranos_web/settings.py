ce13934b56e2564ca8e1621f10e3aacad36799ac
Vadik Alperovich <vadikalperovich@mail.ru>
2020-06-14 23:16:50 +0300

    minor fixes, gravatar added

--- app/sopranos_web/settings.py
+++ app/sopranos_web/settings.py
@@ -37,9 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    # 'board.apps.BoardConfig'
     'board',
 ]
+
+GRAVATAR_SECURE = True
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
