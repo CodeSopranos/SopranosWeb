dfaf7eef4cf1ab3ef0bc4a4a36626c7950b75e53
Vadik Alperovich <vadikalperovich@mail.ru>
2020-06-16 22:11:05 +0300

    add styles to sighup and forms

--- app/board/handlers/auth.py
+++ app/board/handlers/auth.py
@@ -9,22 +9,28 @@ from django.shortcuts import render, redirect, get_object_or_404
 
 from django.contrib.auth.models import User
 from board.models import Dashboard, Figure
+from board.forms import SignupForm
 
 
 def sign_up(request):
-    form = UserCreationForm(request.POST)
-    if form.is_valid():
-        form.save()
-        username = form.cleaned_data.get('username')
-        password = form.cleaned_data.get('password1')
-        user = authenticate(username=username, password=password)
-        login(request, user)
-        return HttpResponseRedirect(reverse('dashboards'))
+    if request.method == 'POST':
+        logout(request)
+        form = SignupForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('Username')
+            password = form.cleaned_data.get('Password')
+            email    = form.cleaned_data.get('Email')
+            user = authenticate(username=username, password=password, email=email)
+            login(request, user)
+            return HttpResponseRedirect(reverse('dashboards'))
     return HttpResponse(render(request, 'board/signup.html', {'form': form}))
+
 
 def sign_out(request):
     logout(request)
     return HttpResponseRedirect(reverse('dashboards'))
+
 
 def sign_in(request):
     if request.method == 'POST':
