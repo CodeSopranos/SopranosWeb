1f961a655f08130f162070bb1c5a65f89720a112
Vadik Alperovich <vadikalperovich@mail.ru>
2020-06-18 11:12:17 +0300

    fixes admin and login

--- app/board/admin.py
+++ app/board/admin.py
@@ -6,36 +6,6 @@ from .models import *
 from .models import *
 
 
-# class PostInline(admin.TabularInline):
-#     def has_add_permission(self, request, obj):
-#         return False
-#
-#     model = Post
-#     fields = ('subject', 'text', 'created_at', 'is_modified')
-#     readonly_fields = ('subject', 'text', 'created_at', 'is_modified')
-#     ordering = ('-created_at',)
-#     show_change_link = True
-#
-#
-# class PostAdmin(admin.ModelAdmin):
-#     def has_add_permission(self, request):
-#         return False
-#
-#     def get_queryset(self, request):
-#         return super().get_queryset(request).annotate(author=F('blog__author__username'))
-#
-#     def author(self, obj):
-#         return obj.author
-#     author.admin_order_field = 'author'
-#
-#     list_display = ('author', 'subject', 'created_at', 'is_modified')
-#     ordering = ('-created_at',)
-#     list_display_links = ('subject',)
-#     fields = ('blog', 'subject', 'text', 'created_at', 'updated_at')
-#     readonly_fields = ('blog', 'created_at', 'updated_at')
-#
-
-
 class DashboardAdmin(admin.ModelAdmin):
     def has_add_permission(self, request):
         return False
@@ -49,8 +19,8 @@ class DashboardAdmin(admin.ModelAdmin):
 
     list_display = ('name', 'theme', 'owner')
     ordering = ('name',)
-    fields = ('owner', 'name', 'created_at')
-    readonly_fields = ('created_at',)
+    fields = ('owner', 'name', 'created_at', 'private')
+    readonly_fields = ('created_at', 'owner')
     view_on_site = True
     # inlines = (PostInline,)
 
